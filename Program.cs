b9fa254d9aaca46d61b592904fdd4016467ccd15
mert <mdereli.37@gmail.com>
2022-07-10 18:44:46 +0300

    update

--- Program.cs
+++ Program.cs
@@ -9,8 +9,8 @@ using(var db = new ShopContext()){
     var user = db.Users.Where(x => x.Name == "Mert").FirstOrDefault();
 
     System.Console.WriteLine("User:");
-    System.Console.WriteLine(user.Id);
-    System.Console.WriteLine(user.Name);
+    System.Console.WriteLine("Id:" + user.Id);
+    System.Console.WriteLine("Name:" + user.Name);
 
     // db.Users.Add(new User{
     //     Name = "Mert",
